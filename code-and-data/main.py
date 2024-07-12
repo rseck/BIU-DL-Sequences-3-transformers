ace66495e037388a8cb3b1edad54f53f5341f03f
rseck <esquirar@gmail.com>
2024-07-12 17:29:23 +0300

    added print to file of loss and sampling and model files and graph

--- code-and-data/main.py
+++ code-and-data/main.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import json
+import os.path
+
 import torch
 from torch import optim
 
@@ -8,10 +11,35 @@ import lm
 from transformer import TransformerLM
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
+DEBUG = True
+
+
+def get_file_name(seq_len, batch_size, data_path, n_layers, n_heads, embed_size, mlp_hidden_size, learning_rate,
+                  gradient_clipping, weight_decay, num_batches_to_train, use_scheduler):
+    s = seq_len
+    b = batch_size
+    dp = data_path
+    nl = n_layers
+    nh = n_heads
+    es = embed_size
+    mhs = mlp_hidden_size
+
+    lr = learning_rate
+    gc = gradient_clipping
+    wd = weight_decay
+
+    nbtt = num_batches_to_train
+    us = use_scheduler
+
+    return (f"s_{s}_b_{b}_dp_{dp}_nl_{nl}_nh_{nh}_es_{es}_mhs_{mhs}_lr_{lr}_gc_{gc}_wd_{wd}_nbtt_{nbtt}_us_{us}".
+            replace('/', ''))
+
+
+def main():
     seq_len = 128
     batch_size = 64
     data_path = "data/"
+    results_path = "results"
     n_layers = 6
     n_heads = 6
     embed_size = 192
@@ -21,8 +49,16 @@ if __name__ == "__main__":
     gradient_clipping = 1.0
     weight_decay = 0.01
 
-    num_batches_to_train = 50000
+    num_batches_to_train = 100 if DEBUG else 50000
+
     use_scheduler = True
+
+    run_file_name = get_file_name(seq_len, batch_size, data_path, n_layers, n_heads, embed_size,
+                                  mlp_hidden_size, learning_rate, gradient_clipping, weight_decay,
+                                  num_batches_to_train, use_scheduler)
+    loss_file_name = os.path.join(results_path, 'losses' + run_file_name + '.json')
+    sampling_file_name = os.path.join(results_path, 'sampling' + run_file_name + '.json')
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     tokenizer, tokenized_data = data.load_data(data_path)
@@ -51,6 +87,7 @@ if __name__ == "__main__":
 
     num_batches = 0
     losses = []
+    samples = []
     while num_batches < num_batches_to_train:
         for batch in data.batch_items(data_iter, batch_size):
             if num_batches >= num_batches_to_train:
@@ -89,8 +126,18 @@ if __name__ == "__main__":
                         )
                         model.train()
                         print(f"Model sample: '''{sampled}'''")
+                        samples.append(sampled)
                         if num_batches % 10000 == 0:
-                            torch.save(model.state_dict(), f"llm_model_{num_batches}.pth")
+                            torch.save(model.state_dict(),
+                                       os.path.join(results_path, f"llm_model_{num_batches}_{run_file_name}.pth"))
                     print("")
-    plt.plot(losses) # noqa
-    plt.savefig(f"llm_losses.png")
+    plt.plot(losses)  # noqa
+    plt.savefig(os.path.join(results_path, f"llm_losses_{run_file_name}.png"))
+    with open(loss_file_name, "a") as output:
+        json.dump(losses, output, indent=4)
+    with open(sampling_file_name, mode="a", encoding='utf-8') as output:
+        json.dump(samples, output, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    main()
