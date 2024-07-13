f7c136fd4e62fee8a5ddbbaec9a00b6a43787e28
rseck <esquirar@gmail.com>
2024-07-13 13:33:55 +0300

    proper initilization

--- code-and-data/transformer.py
+++ code-and-data/transformer.py
@@ -8,12 +8,12 @@ from utils import get_module_device
 
 class TransformerDecoderBlock(nn.Module):
     def __init__(
-        self,
-        n_heads: int,
-        embed_size: int,
-        mlp_hidden_size: int,
-        max_context_len,
-        with_residuals: bool = False,
+            self,
+            n_heads: int,
+            embed_size: int,
+            mlp_hidden_size: int,
+            max_context_len,
+            with_residuals: bool = False,
     ):
         super().__init__()
         self.causal_attention = attention.CausalSelfAttention(
@@ -60,14 +60,14 @@ class Embed(nn.Module):
 
 class TransformerLM(nn.Module):
     def __init__(
-        self,
-        n_layers: int,
-        n_heads: int,
-        embed_size: int,
-        max_context_len: int,
-        vocab_size: int,
-        mlp_hidden_size: int,
-        with_residuals: bool,
+            self,
+            n_layers: int,
+            n_heads: int,
+            embed_size: int,
+            max_context_len: int,
+            vocab_size: int,
+            mlp_hidden_size: int,
+            with_residuals: bool,
     ):
         super().__init__()
         self.embed = Embed(vocab_size, embed_size, max_context_len)
@@ -97,18 +97,20 @@ class TransformerLM(nn.Module):
         return logits
 
     def init_weights(self):
-        for pn, p in self.named_parameters():
-            if isinstance(p, nn.LayerNorm) or isinstance(p, nn.Linear) or isinstance(p, nn.Embedding):
-                print("something actually got custom initialized")
-            if isinstance(p, nn.LayerNorm):
-                torch.nn.init.zeros_(p.bias)
-                torch.nn.init.ones_(p.weight)
-            elif isinstance(p, nn.Linear):
-                torch.nn.init.xaviar_normal_(p.weight)
-                if p.bias is not None:
-                    torch.nn.init.xaviar_normal_(p.bias)
-            elif isinstance(p, nn.Embedding):
-                torch.nn.init.xaviar_normal_(p.weight)
+        for name, module in self.named_modules():
+            # print(f"Module name: {name}")
+            init_module(module)
+            # print(module)
+            if isinstance(module, nn.ModuleList):
+                for sub_name, sub_module in module.named_modules():
+                    # print(f"sub_Module name: {sub_name}")
+                    init_module(sub_module)
+                    # print(sub_module)
+                    if isinstance(sub_module, nn.ModuleList):
+                        for sub_sub_name, sub_sub_module in sub_module.named_modules():
+                            # print(f"sub_sub_Module name: {sub_sub_name}")
+                            init_module(sub_sub_module)
+                            # print(sub_sub_module)
 
     def sample_continuation(self, prefix: list[int], max_tokens_to_generate: int) -> list[int]:
         feed_to_lm = prefix[:]
@@ -148,3 +150,17 @@ class TransformerLM(nn.Module):
                 generated.append(sampled_token)
                 feed_to_lm.append(sampled_token)
         return generated
+
+
+def init_module(p):
+    # if isinstance(p, torch.nn.LayerNorm) or isinstance(p, nn.Linear) or isinstance(p, nn.Embedding):
+    # print("something actually got custom initialized")
+    if isinstance(p, nn.LayerNorm):
+        torch.nn.init.zeros_(p.bias)
+        torch.nn.init.ones_(p.weight)
+    elif isinstance(p, nn.Linear):
+        torch.nn.init.xavier_normal_(p.weight)
+        if p.bias is not None:
+            torch.nn.init.normal_(p.bias)
+    elif isinstance(p, nn.Embedding):
+        torch.nn.init.xavier_normal_(p.weight)
