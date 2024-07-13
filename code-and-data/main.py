7277bf0bf145a3e0d9a057d52767f6c0682084c8
rseck <esquirar@gmail.com>
2024-07-13 13:33:11 +0300

    half the batches number

--- code-and-data/main.py
+++ code-and-data/main.py
@@ -49,7 +49,7 @@ def main():
     gradient_clipping = 1.0
     weight_decay = 0.01
 
-    num_batches_to_train = 100 if DEBUG else 50000
+    num_batches_to_train = 100 if DEBUG else 25000
 
     use_scheduler = True
 
