c9a552a324d296fba4ae015966660cd45454be62
Yedidya Kfir <yedidyakfir@gmail.com>
2024-07-19 11:16:11 +0300

    finish loading

--- code-and-data/analyze.py
+++ code-and-data/analyze.py
@@ -1,6 +1,7 @@
+import torch
 from torch import Tensor
 from attention import CausalSelfAttention, self_attention_layer
-
+from pathlib import Path
 
 def heat_map_attention(x: Tensor, attention_layer: CausalSelfAttention):
     for kqv_matrix in attention_layer.kqv_matrices:
@@ -8,5 +9,13 @@ def heat_map_attention(x: Tensor, attention_layer: CausalSelfAttention):
 
 
 def main(model_path: str):
-    torch.load
+    # load model with cpu and create heat map from the attention heads
+    model = torch.load(model_path)
+    for layer in model.layers:
+        if isinstance(layer, CausalSelfAttention):
+            heat_map_attention(layer.x, layer)
 
+
+if __name__ == "__main__":
+    path = Path() / "results for yedidia" / "loss_0.25_llm_model_25000_s_128_b_64_dp_data_nl_6_nh_6_es_192_mhs_768_lr_0.0005_gc_1.0_wd_false_nbtt_25000_us_False.pth"
+    main(path)
